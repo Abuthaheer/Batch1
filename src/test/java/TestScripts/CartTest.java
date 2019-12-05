aff7ac4fd0d51a3592e6e52661002695edd9f02a
Abuthaheer Sameemullha <35775@Hexaware.com>
2019-12-05 10:49:21 +0530

    UserA Addded

---
+++ src/test/java/TestScripts/CartTest.java
@@ -0,0 +1,27 @@
+package TestScripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObject.HomePageObject;
+
+public class HomePageTest {
+	
+	HomePageObject hp;
+
+	public HomePageTest() {
+		hp = new HomePageObject();
+	}
+
+	@Test
+	public void verifyTabNavigation() {
+		hp.clickWomen();
+		hp.clickWomen();
+		Assert.assertTrue(hp.getTitlte().contains("Women"));
+		hp.clickDresses();
+		Assert.assertTrue(hp.getTitlte().contains("Dresses"));
+		hp.clickTshirt();
+		Assert.assertTrue(hp.getTitlte().contains("T-shirts"));
+	}
+
+}
