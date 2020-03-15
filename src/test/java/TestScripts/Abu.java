d37d17623724408dfe5f80f9d847851fa85a741e
Abuthaheer Sameemullha <35775@Hexaware.com>
2020-03-15 11:07:06 +0530

    Created a new Test File - UserA

---
+++ src/test/java/TestScripts/Abu.java
@@ -0,0 +1,30 @@
+package TestScripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObject.HomePageObject;
+
+public class HomePageTest {
+	
+	HomePageObject hp;
+
+	public HomePageTest() {
+
+	hp2 = new HomePageObject();
+	hp1 = new HomePageObject();
+
+	}
+
+	@Test
+	public void verifyTabNavigation() {
+		hp.clickWomen();
+		hp.clickWomen();
+		Assert.assertTrue(hp.getTitlte().contains("Women"));
+		hp.clickDresses();
+		Assert.assertTrue(hp.getTitlte().contains("Dresses"));
+		hp.clickTshirt();
+		Assert.assertTrue(hp.getTitlte().contains("T-shirts"));
+	}
+
+}
